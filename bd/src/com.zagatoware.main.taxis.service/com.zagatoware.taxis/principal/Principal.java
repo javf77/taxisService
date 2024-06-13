fb9767a81ea0b7425194997da383b5a21ab73115
javf77 <40832153+javf77@users.noreply.github.com>
2024-06-13 11:08:40 -0600

    se agregan los cambios

---
+++ bd/src/com.zagatoware.main.taxis.service/com.zagatoware.taxis/principal/Principal.java
@@ -0,0 +1,25 @@
+package com.zagatoware.taxis.main.principal;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class VentanaConMenuJavaFX extends Application {
+
+    @override
+    public void start(Stage primaryStage) {
+        // Barra del menu principal
+            MenuBar menuBar = new MenuBar();
+
+            
+
+    }
+
+}
